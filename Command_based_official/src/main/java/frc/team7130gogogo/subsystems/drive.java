56437f863c4f57b006dd3f81776153028cf33d45
futureshock7130 <futureshock7130@ms.mingdao.edu.tw>
2020-10-30 17:05:20 +0800

    20201030_2

--- Command_based_official/src/main/java/frc/team7130gogogo/subsystems/drive.java
+++ Command_based_official/src/main/java/frc/team7130gogogo/subsystems/drive.java
@@ -3,10 +3,41 @@
 
 
 package frc.team7130gogogo.subsystems;
-
-import edu.wpi.first.wpilibj.Talon;
+//import com.ctre.phoenix.*;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj.drive.*;
+import frc.team7130gogogo.Constants;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 public class drive extends SubsystemBase {
-    private 
+    
+
+    // hard ware 
+    public TalonFX mleftMaster=new TalonFX(Constants.kLeftDriveMasterId);
+    public TalonFX mrightMaster=new TalonFX(Constants.kRightDriveMasterId);
+    public TalonFX mleftslave=new TalonFX(Constants.kLeftDriveSlaveId);
+    public TalonFX mrightslave=new TalonFX(Constants.kRightDriveSlaveId);
+    private final DifferentialDrive mDrive = new DifferentialDrive(mleftMaster, mrightMaster);
+
+    // 變速箱變換 
+     
+   
+    
+    private void Drive() {
+        public int chasis_encoder=mleftMaster.getSelectedSensorPosition();
+        mleftMaster.setInverted(true);
+        mleftslave.setInverted(true);
+        mleftslave.follow(mleftMaster);
+        mrightslave.follow(mrightMaster);
+        mDrive.arcadeDrive(xSpeed, zRotation);
+    
+    }
 }
+
+
+
+
+
 
--- Command_based_official/src/main/java/frc/team7130gogogo/subsystems/suckballs.java
+++ Command_based_official/src/main/java/frc/team7130gogogo/subsystems/suckballs.java
@@ -1,3 +1,2 @@
 package frc.team7130gogogo.subsystems;
 
-import frc.team7130gogogo.
